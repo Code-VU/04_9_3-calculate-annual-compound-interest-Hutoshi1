6c2327d2012f65bdcb7004a15a2d16c8b7872ff9
Tony Davis <Tony.Davis@vu.com>
2024-01-25 11:33:29 -0600

    completed functions

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,10 +1,53 @@
+'''
+Principle (amount): 1200
+Time:               2
+Rate:               5.4
+Compound Interest:  133.1
+---
+Principle (amount): 12345
+Time:               8
+Rate:               6.7
+Compound Interest:  8394.89
+---
+Principle (amount): 50
+Time:               1
+Rate:               1
+Compound Interest:  0.5
+'''
+
+'''
+A = P( 1 + R/100)^T
+
+CI = A - P
+
+A is Amount
+P is Principal Amount
+R is Interest Rate
+T is Time Span in Years
+CI is Compound Interest
+'''
+
 def calculateCompoundInterest():
-    
-# This first 3 lines are provided for yougetACompoundInterest()
-# This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
+    numOfClients = 3
+
+    while numOfClients > 0:
+
+        client_one_principal = float(input("Principle (amount): "))
+        client_one_time =      float(input("Time:               "))
+        client_one_rate =      float(input("Rate:               "))
+
+        compoundIntCalc(client_one_principal, client_one_time, client_one_rate)
+        if numOfClients > 1:
+            print("---")
+        numOfClients = numOfClients - 1
+
+def compoundIntCalc(principal, time, rate) ->float:
+
+    amount = principal*( 1 + rate/100)**time
+    compoundIntTotal = round(amount - principal, 2)
+
+    print(f"Compound Interest: {compoundIntTotal}")
+
  #print("Compound Interest: "+str(intrest))
 
     # end assignment
